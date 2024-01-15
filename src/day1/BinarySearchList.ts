b2eec943a5a70e42ea6ab27e7709271ad0af96c9
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2024-01-14 23:27:19 -0500

    implemented basic binary search

---
+++ src/day1/BinarySearchList.ts
@@ -0,0 +1,16 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+    let low = 0
+    let high = haystack.length
+    while (low < high) {
+        const midpoint = Math.floor(low + (high - low) / 2)
+        const value = haystack[midpoint]
+        if (value === needle) {
+            return true
+        } else if (value > needle) {
+            high = midpoint 
+        } else if (value < needle) {
+            low = midpoint + 1 
+        }
+    }
+    return false
+}
